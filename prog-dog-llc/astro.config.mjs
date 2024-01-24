7390439c9658247339ddbca05cb17e4472ca2fe6
Chalse O <6561950+chalseoko@users.noreply.github.com>
2024-01-24 08:24:50 -0800

    Add basic styling for page

--- prog-dog-llc/astro.config.mjs
+++ prog-dog-llc/astro.config.mjs
@@ -2,15 +2,16 @@ import react from "@astrojs/react";
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
-
-import formDebug from "@astro-utils/forms/dist/integration.js";
-import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
   output: "server",
   site: "https://www.progdogproductions.com",
   adapter: vercel(),
-  integrations: [react(), tailwind(), icon(), robotsTxt(), formDebug ],
+  integrations: [react(), icon({
+    include: {
+      logo: ['facebook', 'youtube', 'xtend']
+    }
+  }), robotsTxt()],
   renderers: ['@astrojs/renderer-react']
 });
